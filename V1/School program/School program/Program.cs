90623ca015c7ded75c081fdc38b1fa0b56ecab57
AndjeloR <obervk1003@gmail.com>
2025-07-03 16:08:58 +0300

    V1 version

---
+++ V1/School program/School program/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace School_program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TODO tuka she budat vuvedeni imenata na uchitelite,predmetite i she bude wiknat metoda ScheduleManager
+        }
+    }
+}
